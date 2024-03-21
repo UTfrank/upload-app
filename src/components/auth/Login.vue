1023eedd1a2b0c8f0457154d248cba93c0ea7d01
frank john <frankjoe261@gmail.com>
2024-03-21 19:14:59 +0100

    finished the login form

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+<template lang="">
+  <div class="mb-5 font-poppins">
+    <label for="email" class=" mb-[10px] text-sm text-gray-200 leading-5">Email Address</label>
+    <input type="email" v-model="email" id="email" class="py-[15px] px-[22px] rounded-[35px] w-full border border-gray-200 text-gray-400 bg-transparent" placeholder="example@mail.com">
+  </div>
+  <div class="mb-5 font-poppins">
+    <label for="password" class="mb-[10px] text-sm text-gray-200 leading-5">Password</label>
+    <input type="password" v-model="password" id="password" class="py-[15px] px-[22px] rounded-[35px] w-full border border-gray-200 text-gray-400 bg-transparent" placeholder="*********">
+  </div>
+  <div class="mb-5 font-poppins flex items-center gap-x-1">
+    <input type="checkbox" v-model="checked" id="checkbox" class="h-[19px] w-[19px] bg-transparent rounded-md">
+    <label for="checkbox" class="text-sm text-gray-200 leading-5">Remember me</label>
+  </div>
+  <div class="mb-5 font-poppins flex items-center gap-x-1">
+    <button type="submit" class="font-semibold text-base leading-[28px] text-center w-full bg-blue-600 text-white py-4 rounded-[50px]">Login</button>
+  </div>
+</template>
+<style scoped>
+  
+</style>
---
+++ src/components/layout/FormLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import Login from '../auth/Login.vue';
+</script>
+
+<template>
+  <div class="w-full h-screen bg-blue-950 flex">
+    <div class="lg:w-1/2 hidden lg:flex justify-center items-center">
+      <img src="../../assets/form-img.png" alt="lady smiling" class="w-7/12">
+    </div>
+    <div class="lg:w-1/2 md:w-full flex justify-center items-center">
+      <div class="lg:w-8/12 md:w-6/12 w-10/12">
+        <h2 class="font-poppins font-semibold text-white md:text-4xl text-[28px] lg:mb-[60px] mb-5">Welcome back 👋</h2>
+        <Login />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
